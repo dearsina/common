c5b00a2b693a80249ca54ba83c6f92a55663e748
Sina Bahrami <github@pixbox.co.uk>
2021-05-28 09:46:42 +0200

    Update the WebSocket limit.

--- src/PA.php
+++ src/PA.php
@@ -335,7 +335,7 @@ class PA {
 
 		/**
 		 * For some reason, this doesn't work. So we're doing a hack-y version below,
-		 * where data > 100kb is stored in a file and the file link is sent instead.
+		 * where data > 1kb is stored in a file and the file link is sent instead.
 		 * This is to avoid hitting shell_exec max string limits.
 		 *
 		 * StackOverflow ticket:
@@ -357,12 +357,12 @@ class PA {
 		# Upgrade the connection (not exactly sure why)
 		$cmd .= "\$client->upgrade(\"/\");";
 
-		# If the data being sent is larger than ~100kb, create a tmp file and send the file link instead
-		if(strlen($data) > 100000){
-			//if more than ~100kb is being sent
+		# If the data being sent is larger than 900 chars, create a tmp file and send the file link instead
+		if(strlen($data) > 900){
+			//if more than 900 chars is being sent
 			$filename = $_ENV['tmp_dir'].rand();
 			file_put_contents($filename, $data);
-			$cmd .= "\$client->push(urldecode(file_get_contents(\"{$filename}\")));unset(\"{$filename}\");";
+			$cmd .= "\$client->push(urldecode(file_get_contents(\"{$filename}\")));";//unset(\"{$filename}\");";
 		}
 
 		# For short messages, just sent the data
