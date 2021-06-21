90dc69dd50dcbb55edc71c81779abaf2efd34d00
Sina Bahrami <github@pixbox.co.uk>
2021-06-21 21:35:48 +0200

    Comment update.

--- src/Exception/BadRequest.php
+++ src/Exception/BadRequest.php
@@ -14,7 +14,7 @@ class BadRequest extends Prototype {
 	 *
 	 * @param string          $public_message  The message you want to sent back to the API requester
 	 * @param string|null     $private_message The message you want to log for the admins.
-	 * @param int             $code            The http response code to issue with this error, default 401
+	 * @param int             $code            The http response code to issue with this error, default 400
 	 * @param \Exception|null $previous
 	 */
 	public function __construct(string $public_message, ?string $private_message = NULL, $code = 400, \Exception $previous = NULL)
