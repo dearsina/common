9e4a9473ebb3973e14d556d29e2c779777a2c362
Sina Bahrami <github@pixbox.co.uk>
2023-01-11 18:33:21 +0200

    Remove ping/pong from websockets. Also removed requirement to match IP address on websocket FD registration.

--- src/WebSocketServer/WebSocketServer.php
+++ src/WebSocketServer/WebSocketServer.php
@@ -46,7 +46,7 @@ class WebSocketServer extends Prototype {
 	 */
 	private $server_id;
 
-	public function __construct ()
+	public function __construct()
 	{
 		# Ensure this class is only accessed from the command line
 		if(!str::runFromCLI()){
@@ -61,15 +61,16 @@ class WebSocketServer extends Prototype {
 	 *
 	 * @param $msg
 	 */
-	private function alert($msg){
-		$line = "\r\n".date("Y-m-d H:i:s")." | ".$msg;
+	private function alert($msg)
+	{
+		$line = "\r\n" . date("Y-m-d H:i:s") . " | " . $msg;
 
 		if(!$log_file = $_ENV['websocket_log_file']){
 			// If for some reason the $_ENV variables haven't been loaded
 			$log_file = "/var/log/swoole.alert";
 			// Have a backup
 		}
-		
+
 		$f = fopen($log_file, "a");
 		fwrite($f, $line);
 		fclose($f);
@@ -91,13 +92,13 @@ class WebSocketServer extends Prototype {
 	public function start(): void
 	{
 		# Ensure the server isn't already running
-		if($this->serverAlreadyRunning()) {
+		if($this->serverAlreadyRunning()){
 			$this->log->success("The server is already running.");
 			return;
 		}
 
 		# Set up the server
-		$this->external_server = new \Swoole\WebSocket\Server($_ENV['websocket_external_ip'], $_ENV['websocket_external_port'],SWOOLE_BASE, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+		$this->external_server = new \Swoole\WebSocket\Server($_ENV['websocket_external_ip'], $_ENV['websocket_external_port'], SWOOLE_BASE, SWOOLE_SOCK_TCP | SWOOLE_SSL);
 
 		# Set the SSL keys
 		$this->external_server->set([
@@ -112,7 +113,7 @@ class WebSocketServer extends Prototype {
 			"daemonize" => true,
 
 			# Raise the message size limit from the default 2mb to 32mb
-			"package_max_length" => 32 * 1024 * 1024
+			"package_max_length" => 32 * 1024 * 1024,
 		]);
 
 		# Generate the server ID
@@ -160,7 +161,8 @@ class WebSocketServer extends Prototype {
 	 *
 	 * @param \Swoole\WebSocket\Server $server
 	 */
-	public function onStart(\Swoole\WebSocket\Server $server){
+	public function onStart(\Swoole\WebSocket\Server $server)
+	{
 		$message = "Swoole WebSocket Server [{$this->server_id}] has started at wss://{$_ENV['websocket_external_ip']}:{$_ENV['websocket_external_port']}";
 
 		# Log it to the admins
@@ -170,7 +172,7 @@ class WebSocketServer extends Prototype {
 		$this->log->info([
 			"icon" => "server",
 			"title" => "Swoole WebSocket Server",
-			"message" => $message
+			"message" => $message,
 		]);
 	}
 
@@ -180,7 +182,8 @@ class WebSocketServer extends Prototype {
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param int                      $fd
 	 */
-	public function onInternalConnect(\Swoole\WebSocket\Server $server, int $fd) {
+	public function onInternalConnect(\Swoole\WebSocket\Server $server, int $fd)
+	{
 		$this->alert("Internal connection [{$fd}] attempt.");
 	}
 
@@ -190,7 +193,8 @@ class WebSocketServer extends Prototype {
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param \Swoole\Http\Request     $request
 	 */
-	public function onInternalOpen(\Swoole\WebSocket\Server $server, \Swoole\Http\Request $request) {
+	public function onInternalOpen(\Swoole\WebSocket\Server $server, \Swoole\Http\Request $request)
+	{
 		$this->alert("Internal connection [{$request->fd}] opened.");
 	}
 
@@ -209,7 +213,7 @@ class WebSocketServer extends Prototype {
 	 * @return bool
 	 * @return bool
 	 */
-	public function onInternalMessage(\Swoole\WebSocket\Server $server, \Swoole\WebSocket\Frame $frame):bool
+	public function onInternalMessage(\Swoole\WebSocket\Server $server, \Swoole\WebSocket\Frame $frame): bool
 	{
 		$this->alert("Internal message from connection [{$frame->fd}]: {$frame->data}");
 
@@ -225,8 +229,9 @@ class WebSocketServer extends Prototype {
 		foreach($data_array['fd'] as $id => $fd){
 			if($server->isEstablished($fd)){
 				$server->push($fd, json_encode($data_array['data']));
-			} else {
-				echo "The [{$fd}] connection is no longer established, thus the following message was not sent to them:\r\n".json_encode($data_array['data']);
+			}
+			else {
+				echo "The [{$fd}] connection is no longer established, thus the following message was not sent to them:\r\n" . json_encode($data_array['data']);
 			}
 
 		}
@@ -240,7 +245,8 @@ class WebSocketServer extends Prototype {
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param int                      $fd
 	 */
-	public function onInternalClose(\Swoole\WebSocket\Server $server, int $fd){
+	public function onInternalClose(\Swoole\WebSocket\Server $server, int $fd)
+	{
 		$this->alert("Internal connection [{$fd}] closed.");
 	}
 
@@ -250,12 +256,29 @@ class WebSocketServer extends Prototype {
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param int                      $fd
 	 */
-	public function onExternalConnect(\Swoole\WebSocket\Server $server, int $fd) {
+	public function onExternalConnect(\Swoole\WebSocket\Server $server, int $fd)
+	{
 		$this->alert("External connection [{$fd}] attempt.");
 	}
 
 	/**
 	 * When an external connection has opened.
+	 *
+	 * $request->server contains the following:
+	 * <code>
+	 * [
+	 *    'request_method' => 'GET',
+	 *    'request_uri' => '/9832d6dc-9093-4233-af47-ebb878a48ba6',
+	 *    'path_info' => '/9832d6dc-9093-4233-af47-ebb878a48ba6',
+	 *    'request_time' => 1673443811,
+	 *    'request_time_float' => 1673443811.507008,
+	 *    'server_protocol' => 'HTTP/1.1',
+	 *    'server_port' => 4043,
+	 *    'remote_port' => 51574,
+	 *    'remote_addr' => '169.0.137.120', // Will always be an IPv4 address
+	 *    'master_time' => 1673443811,
+	 * ]
+	 * </code>
 	 *
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param \Swoole\Http\Request     $request
@@ -264,9 +287,10 @@ class WebSocketServer extends Prototype {
 	 * @throws \Exception
 	 * @throws \Exception
 	 */
-	public function onExternalOpen(\Swoole\WebSocket\Server $server, \Swoole\Http\Request $request): bool {
+	public function onExternalOpen(\Swoole\WebSocket\Server $server, \Swoole\Http\Request $request): bool
+	{
 		# The browser will send along the connection ID
-		$connection_id = substr($request->server['request_uri'],1);
+		$connection_id = substr($request->server['request_uri'], 1);
 		//The request_uri includes a prefixed "/"
 
 		# Make sure the connection ID exists, is valid and is for the right IP
@@ -275,18 +299,22 @@ class WebSocketServer extends Prototype {
 			"set" => [
 				"server_id" => $this->server_id,
 				"fd" => $request->fd,
-				"opened" => "NOW()"
+				"opened" => "NOW()",
 			],
 			"where" => [
 				"closed" => NULL,
-				"ip" => $request->server['remote_addr'],
+				//				"ip" => $request->server['remote_addr'],
+				/**
+				 * For now, we cannot use the IP address because
+				 * it is always an IPv4 address, while the connection
+				 * table does at times use an IPv6 address.
+				 */
 			],
 			"id" => $connection_id,
 			"reconnect" => true,
 			"user_id" => false,
 		])){
 			//If the connection is not valid
-
 			var_dump($this->log->getAlerts());
 			$this->log->clearAlerts();
 			return false;
@@ -298,24 +326,17 @@ class WebSocketServer extends Prototype {
 	}
 
 	/**
-	 * When an external connection sends a message.
+	 * When an external connection sends a message to the server,
+	 * it is caught here. Nothing is done with it at the moment.
 	 * Jury still out on whether AJAX or WebSockets should be used
 	 * to send messages from a client browser to the server.
 	 *
 	 * @param \Swoole\WebSocket\Server $server
 	 * @param \Swoole\WebSocket\Frame  $frame
 	 */
-	public function onExternalMessage(\Swoole\WebSocket\Server $server, \Swoole\WebSocket\Frame $frame) {
+	public function onExternalMessage(\Swoole\WebSocket\Server $server, \Swoole\WebSocket\Frame $frame): void
+	{
 		$this->alert("External message from connection [{$frame->fd}]: {$frame->data}");
-
-		# If we receive a ping, return with a pong
-		if($frame->data === "ping"){
-			$server->push($frame->fd, json_encode([
-				"success" => true,
-				"pong" => true,
-			]));
-		}
-		// This is used to check if the browser can receive incoming WebSocket messages
 	}
 
 	/**
@@ -325,13 +346,14 @@ class WebSocketServer extends Prototype {
 	 * @return bool
 	 * @throws \Exception
 	 */
-	public function onExternalClose (\Swoole\WebSocket\Server $server, int $fd): bool {
+	public function onExternalClose(\Swoole\WebSocket\Server $server, int $fd): bool
+	{
 
 		# Mark the connection as closed on the database
 		if(!$this->sql->update([
 			"table" => "connection",
 			"set" => [
-				"closed" => "NOW()"
+				"closed" => "NOW()",
 			],
 			"where" => [
 				"server_id" => $this->server_id,
@@ -339,7 +361,7 @@ class WebSocketServer extends Prototype {
 			],
 			"user_id" => false,
 			"limit" => 1,
-			"reconnect" => true
+			"reconnect" => true,
 		])){
 			//The connection close was not saved
 			var_dump($this->log->getAlerts());
@@ -355,72 +377,72 @@ class WebSocketServer extends Prototype {
 
 
 /**
-object(Swoole\Http\Request)#24 (8) {
-["fd"]=>
-int(1)
-["header"]=>
-array(12) {
-["host"]=>
-string(28) "registerofmembers.co.uk:4043"
-["connection"]=>
-string(7) "Upgrade"
-["pragma"]=>
-string(8) "no-cache"
-["cache-control"]=>
-string(8) "no-cache"
-["user-agent"]=>
-string(115) "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
-["upgrade"]=>
-string(9) "websocket"
-["origin"]=>
-string(35) "https://app.registerofmembers.co.uk"
-["sec-websocket-version"]=>
-string(2) "13"
-["accept-encoding"]=>
-string(17) "gzip, deflate, br"
-["accept-language"]=>
-string(62) "en-GB,en;q=0.9,en-US;q=0.8,nb;q=0.7,sv;q=0.6,la;q=0.5,no;q=0.4"
-["sec-websocket-key"]=>
-string(24) "mditpwx7MZazIDiVZHMylg=="
-["sec-websocket-extensions"]=>
-string(42) "permessage-deflate; client_max_window_bits"
-}
-["server"]=>
-array(10) {
-["request_method"]=>
-string(3) "GET"
-["request_uri"]=>
-string(27) "/ui9d070pnpnhnq0togicn6sifc"
-["path_info"]=>
-string(27) "/ui9d070pnpnhnq0togicn6sifc"
-["request_time"]=>
-int(1587559801)
-["request_time_float"]=>
-float(1587559801.4001)
-["server_protocol"]=>
-string(8) "HTTP/1.1"
-["server_port"]=>
-int(4043)
-["remote_port"]=>
-int(55358)
-["remote_addr"]=>
-string(13) "105.242.32.54"
-["master_time"]=>
-int(1587559800)
-}
-["cookie"]=>
-array(1) {
-["PHPSESSID"]=>
-string(26) "5b3312js9eeellfeh5c9mr2nll"
-}
-["get"]=>
-NULL
-["files"]=>
-NULL
-["post"]=>
-NULL
-["tmpfiles"]=>
-NULL
-}
-
+ * object(Swoole\Http\Request)#24 (8) {
+ * ["fd"]=>
+ * int(1)
+ * ["header"]=>
+ * array(12) {
+ * ["host"]=>
+ * string(28) "registerofmembers.co.uk:4043"
+ * ["connection"]=>
+ * string(7) "Upgrade"
+ * ["pragma"]=>
+ * string(8) "no-cache"
+ * ["cache-control"]=>
+ * string(8) "no-cache"
+ * ["user-agent"]=>
+ * string(115) "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163
+ * Safari/537.36"
+ * ["upgrade"]=>
+ * string(9) "websocket"
+ * ["origin"]=>
+ * string(35) "https://app.registerofmembers.co.uk"
+ * ["sec-websocket-version"]=>
+ * string(2) "13"
+ * ["accept-encoding"]=>
+ * string(17) "gzip, deflate, br"
+ * ["accept-language"]=>
+ * string(62) "en-GB,en;q=0.9,en-US;q=0.8,nb;q=0.7,sv;q=0.6,la;q=0.5,no;q=0.4"
+ * ["sec-websocket-key"]=>
+ * string(24) "mditpwx7MZazIDiVZHMylg=="
+ * ["sec-websocket-extensions"]=>
+ * string(42) "permessage-deflate; client_max_window_bits"
+ * }
+ * ["server"]=>
+ * array(10) {
+ * ["request_method"]=>
+ * string(3) "GET"
+ * ["request_uri"]=>
+ * string(27) "/ui9d070pnpnhnq0togicn6sifc"
+ * ["path_info"]=>
+ * string(27) "/ui9d070pnpnhnq0togicn6sifc"
+ * ["request_time"]=>
+ * int(1587559801)
+ * ["request_time_float"]=>
+ * float(1587559801.4001)
+ * ["server_protocol"]=>
+ * string(8) "HTTP/1.1"
+ * ["server_port"]=>
+ * int(4043)
+ * ["remote_port"]=>
+ * int(55358)
+ * ["remote_addr"]=>
+ * string(13) "105.242.32.54"
+ * ["master_time"]=>
+ * int(1587559800)
+ * }
+ * ["cookie"]=>
+ * array(1) {
+ * ["PHPSESSID"]=>
+ * string(26) "5b3312js9eeellfeh5c9mr2nll"
+ * }
+ * ["get"]=>
+ * NULL
+ * ["files"]=>
+ * NULL
+ * ["post"]=>
+ * NULL
+ * ["tmpfiles"]=>
+ * NULL
+ * }
  */
