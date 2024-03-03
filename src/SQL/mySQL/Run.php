e03198dd447ffdfd29e5641090221cfefb9c9a17
Sina Bahrami <github@pixbox.co.uk>
2024-03-03 10:45:49 +0200

    Improve error handling when building mySQL queries.

--- src/SQL/mySQL/Run.php
+++ src/SQL/mySQL/Run.php
@@ -8,76 +8,93 @@ use App\Common\str;
 
 class Run extends Common {
 	/**
-	 * Runs a SQL query.
+	 * The number of times to try to re-run a query
+	 * if the connection is lost.
+	 */
+	const MAX_TRIES = 5;
+
+	/**
+	 * Runs a SQL query. Returns an array with the following
+	 * keys:
+	 * - `query` The SQL query just run
+	 * - `rows` (SELECT only), the rows matching the query
+	 * - `num_rows` (SELECT only) The number of rows found by the query
+	 * - `affected_rows` The number of rows affected by the query
 	 *
-	 * @param string $query
+	 * @param string   $query
+	 * @param int|null $tries
 	 *
 	 * @return array
+	 * @throws \Exception
 	 */
-	public function run(string $query, ?int $tries = 0): array
+	public function run(string $query, ?int $tries = 1): array
 	{
 		# Store the query in a session variable
 		$this->logRun($query);
 
-		$filtered_query = preg_replace("/([\"'])(?:\\\\?+.)*?\\1/", '', $query);
-		// We're only interested in semicolons if they appear outside quoted strings
-		// @link https://stackoverflow.com/a/1060703/429071
+		# Ensure the connection is live
+		$this->ensureConnection();
 
-		if(!$this->mysqli){
-			$this->mysqli = mySQL::getNewConnection();
-		}
+		# Try
+		try {
 
-		if(strpos($filtered_query, ";") && count(array_filter(explode(";", trim($filtered_query)))) > 1){
-			//If there is more than one query to run
+			# Multi-query
+			if($this->isMultiQuery($query)){
+				//If there is more than one query to run
 
-			# Run the multi-query
-			$result = $this->mysqli->multi_query($query);
+				# Run the multi-query
+				if(!$result = @$this->mysqli->multi_query($query)){
+					// Something went wrong
+					throw new \mysqli_sql_exception("SQL multi-query error: " . mysqli_connect_error(), mysqli_connect_errno());
+				}
 
-			# Consume the results so that a query can be run afterwards
-			while($this->mysqli->next_result()){
-				//Otherwise threads will collide
+				# Consume the results so that a query can be run afterwards
+				while($this->mysqli->next_result()) {
+					//Otherwise threads will collide
+				}
 			}
-		}
 
-		else {
-			//if there is only one query to run
+			# Single query (most common)
+			else {
+				//if there is only one query to run
 
-			try {
 				# Run the query
 				if(!$result = @$this->mysqli->query($query)){
 					// Something went wrong
 					throw new \mysqli_sql_exception("SQL query error: " . mysqli_connect_error(), mysqli_connect_errno());
 				}
 			}
+		}
 
-			catch(\mysqli_sql_exception $e){
-				# Grab the error message
-				$message = $e->getMessage();
-				// Grabbing it so that we can optionally add to it
+		catch(\mysqli_sql_exception $e) {
+			# Grab the error message
+			$message = $e->getMessage();
+			// Grabbing it so that we can optionally add to it
 
-				switch($message){
-				case 'MySQL server has gone away':
-				default:
-					# Try again
-					if($tries < 4){
-						# Close the connection
-						$this->mysqli->close();
-						# Sleep
-						sleep($tries);
-						# Create a new connection
-						$this->mysqli = mySQL::getNewConnection();
-						# Count the try
-						$tries++;
-						# Rerun the query
-						return $this->run($query, $tries);
-					}
-					# Note to the log that we tried really hard to make this work
-					$message .= ". Tried to re-run the query {$tries} times.";
-					break;
+			# Common error messages that warrant a re-run
+			switch($message) {
+			case 'MySQL server has gone away':
+			default:
+				# Try again if we haven't tried too many times
+				if($tries <= self::MAX_TRIES){
+					# Close the connection
+					$this->mysqli->close();
+					# Sleep
+					sleep($tries);
+					# Create a new connection
+					$this->mysqli = mySQL::getNewConnection();
+					# Count the try
+					$tries++;
+					# Rerun the query
+					return $this->run($query, $tries);
 				}
 
-				throw new \mysqli_sql_exception($message, $e->getCode(), $e);
+				# Note to the log that we tried really hard to make this work
+				$message .= ". Tried to re-run the query ".self::MAX_TRIES." times.";
+				break;
 			}
+
+			throw new \mysqli_sql_exception($message, $e->getCode(), $e);
 		}
 
 		/**
@@ -89,7 +106,7 @@ class Run extends Common {
 
 		if(is_object($result)){
 			# Store result rows in an array
-			while ($row = $result->fetch_assoc()) {
+			while($row = $result->fetch_assoc()) {
 				$rows[] = $row;
 			}
 
@@ -97,7 +114,9 @@ class Run extends Common {
 
 			# Free up memory
 			$result->close();
-		} else {
+		}
+
+		else {
 			$affected_rows = $this->mysqli->affected_rows;
 		}
 
@@ -105,8 +124,46 @@ class Run extends Common {
 			"query" => $query,
 			"rows" => $rows,
 			"num_rows" => $num_rows,
-			"affected_rows" => $affected_rows
+			"affected_rows" => $affected_rows,
 		];
+	}
+
+	/**
+	 * Returns true if the query string is actually multiple queries.
+	 *
+	 * @param string $query
+	 *
+	 * @return void
+	 * @link https://stackoverflow.com/a/1060703/429071
+	 */
+	private function isMultiQuery(string $query): bool
+	{
+		# We're only interested in semicolons if they appear outside quoted strings
+		$filtered_query = preg_replace("/([\"'])(?:\\\\?+.)*?\\1/", '', $query);
+
+		# The query must include at least one semicolon
+		if(strpos($filtered_query, ";") === false){
+			return false;
+		}
+
+		# The semicolon must divide the query into at least two parts
+		$parts = array_filter(explode(";", trim($filtered_query)));
+		return count($parts) > 1;
+	}
+
+	/**
+	 * Sometimes the mysqli connection is lost.
+	 * This method ensures that if the connection is lost,
+	 * it is re-established.
+	 *
+	 * @return void
+	 * @throws \Exception
+	 */
+	private function ensureConnection(): void
+	{
+		if(!$this->mysqli){
+			$this->mysqli = mySQL::getNewConnection();
+		}
 	}
 
 	/**
@@ -125,8 +182,8 @@ class Run extends Common {
 		$_SESSION['query'] = $query;
 		$_SESSION['queries'][] = [
 			"query_md5" => md5($query),
-			"query" => str_replace(["\r\n"]," ",$query),
-			"backtrace" => str::backtrace(true, false)
+			"query" => str_replace(["\r\n"], " ", $query),
+			"backtrace" => str::backtrace(true, false),
 		];
 		$_SESSION['database_calls']++;
 		$_SESSION['query_timer'] = str::startTimer();
@@ -139,7 +196,7 @@ class Run extends Common {
 			 */
 			global $SESSION;
 			$SESSION['query'] = $query;
-			$SESSION['queries'][] = str_replace(["\r\n"]," ",$query);
+			$SESSION['queries'][] = str_replace(["\r\n"], " ", $query);
 			$SESSION['database_calls']++;
 		}
 	}
