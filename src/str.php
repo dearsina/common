ca47f5d7099f84c03f1a637a43cb023fc81f6d72
Sina Bahrami <github@pixbox.co.uk>
2024-07-24 10:55:47 +0200

    Allow for number as words.

--- src/str.php
+++ src/str.php
@@ -5045,4 +5045,115 @@ EOF;
 
 		return mb_strtoupper($initials);
 	}
+
+	/**
+	 * Given a number, returns the number as words.
+	 * Will also handle decimals.
+	 * Will return NULL if the number is not numeric.
+	 * Will return "zero" if the number is 0.
+	 * Will return "negative" if the number is negative.
+	 *
+	 * @param float|null $number
+	 *
+	 * @return string|null
+	 */
+	public static function numberAsWords(?float $number): ?string
+	{
+		$hyphen      = '-';
+		$conjunction = ' and ';
+		$separator   = ', ';
+		$negative    = 'negative ';
+		$decimal     = ' point ';
+		$dictionary  = [
+			0                   => 'zero',
+			1                   => 'one',
+			2                   => 'two',
+			3                   => 'three',
+			4                   => 'four',
+			5                   => 'five',
+			6                   => 'six',
+			7                   => 'seven',
+			8                   => 'eight',
+			9                   => 'nine',
+			10                  => 'ten',
+			11                  => 'eleven',
+			12                  => 'twelve',
+			13                  => 'thirteen',
+			14                  => 'fourteen',
+			15                  => 'fifteen',
+			16                  => 'sixteen',
+			17                  => 'seventeen',
+			18                  => 'eighteen',
+			19                  => 'nineteen',
+			20                  => 'twenty',
+			30                  => 'thirty',
+			40                  => 'forty',
+			50                  => 'fifty',
+			60                  => 'sixty',
+			70                  => 'seventy',
+			80                  => 'eighty',
+			90                  => 'ninety',
+			100                 => 'hundred',
+			1000                => 'thousand',
+			1000000             => 'million',
+			1000000000          => 'billion'
+		];
+
+		if (!is_numeric($number)) {
+			return false;
+		}
+
+		if ($number < 0) {
+			return $negative . str::numberAsWords(abs($number));
+		}
+
+		$string = $fraction = null;
+
+		if (strpos($number, '.') !== false) {
+			[$number, $fraction] = explode('.', $number);
+		}
+
+		switch (true) {
+		case $number < 21:
+			$string = $dictionary[$number];
+			break;
+		case $number < 100:
+			$tens   = ((int) ($number / 10)) * 10;
+			$units  = $number % 10;
+			$string = $dictionary[$tens];
+			if ($units) {
+				$string .= $hyphen . $dictionary[$units];
+			}
+			break;
+		case $number < 1000:
+			$hundreds  = $number / 100;
+			$remainder = $number % 100;
+			$string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+			if ($remainder) {
+				$string .= $conjunction . str::numberAsWords($remainder);
+			}
+			break;
+		default:
+			$baseUnit = pow(1000, floor(log($number, 1000)));
+			$numBaseUnits = (int) ($number / $baseUnit);
+			$remainder = $number % $baseUnit;
+			$string = str::numberAsWords($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+			if ($remainder) {
+				$string .= $remainder < 100 ? $conjunction : $separator;
+				$string .= str::numberAsWords($remainder);
+			}
+			break;
+		}
+
+		if (null !== $fraction && is_numeric($fraction)) {
+			$string .= $decimal;
+			$words = [];
+			foreach (str_split((string) $fraction) as $number) {
+				$words[] = $dictionary[$number];
+			}
+			$string .= implode(' ', $words);
+		}
+
+		return $string;
+	}
 }
