a7fb53ecd939af59e96211ac3dd8c1aace5b6c34
Sina Bahrami <github@pixbox.co.uk>
2024-11-27 10:45:56 +0200

    Allow for silent bad requests.

--- src/Exception/BadRequest.php
+++ src/Exception/BadRequest.php
@@ -18,11 +18,14 @@ class BadRequest extends Prototype {
 	 * @param string|null     $private_message The message you want to log for the admins.
 	 * @param int             $code            The http response code to issue with this error, default 400
 	 * @param \Exception|null $previous
+	 * @param bool|null       $log
 	 */
-	public function __construct(string $public_message, ?string $private_message = NULL, $code = 400, \Exception $previous = NULL)
+	public function __construct(string $public_message, ?string $private_message = NULL, $code = 400, \Exception $previous = NULL, ?bool $log = true)
 	{
 		$private_message = $private_message ?: $public_message;
-		self::logException("Bad Request", $private_message, $code);
+		if($log){
+			self::logException("Bad Request", $private_message, $code);
+		}
 		parent::__construct(str::isDev() ? $private_message : $public_message, $code, $previous);
 	}
 }
