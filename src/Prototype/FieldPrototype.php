55664dcd088d57ca1b48fff1de8c009a5f7464bf
Sina Bahrami <github@pixbox.co.uk>
2025-06-15 12:23:30 +0200

    Expand on hidden fields feature.

--- src/Prototype/FieldPrototype.php
+++ src/Prototype/FieldPrototype.php
@@ -3,6 +3,15 @@
 namespace App\Common\Prototype;
 
 class FieldPrototype {
+	/**
+	 * If a key ends with [], it will assume the value is an array,
+	 * and store it as JSON.
+	 *
+	 * @param array|null $a
+	 * @param array|null $keys
+	 *
+	 * @return array|null
+	 */
 	public static function hiddenFields(?array $a, ?array $keys = NULL): ?array
 	{
 		if(!$keys){
@@ -10,6 +19,14 @@ class FieldPrototype {
 		}
 
 		foreach($keys as $key){
+			if(substr($key, -2) == "[]"){
+				if(is_array($a[substr($key, 0, -2)])){
+					$a[$key] = json_encode($a[substr($key, 0, -2)]);
+				}
+			}
+			if(!isset($a[$key])){
+				continue;
+			}
 			$fields[] = [
 				"type" => "hidden",
 				"name" => $key,
